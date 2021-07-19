7fd0014d572d9e50f76781e7924af7e092dd32ec
penguinshunya <penguinshunya@hotmail.com>
2021-07-19 23:31:19 +0900

    背景色の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 export const App: React.VFC<{}> = () => {
   return (
-    <>hello, world</>
+    <div style={{
+      backgroundColor: "rgba(0, 0, 0, 0.1)",
+      height: "100vh",
+      width: "100%",
+    }}>
+
+    </div>
   );
 };
 
